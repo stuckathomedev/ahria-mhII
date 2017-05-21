3b505565405d63ad3fc7be1c1e4f52f4f8a7f4b3
Kevin Liu <kevin@potatofrom.space>
2017-05-21 10:10:05 -0400

    Speed up voice slightly

--- cogs/voice/tts.py
+++ cogs/voice/tts.py
@@ -3,7 +3,7 @@ import pyttsx
 
 def speak(text):
     engine = pyttsx.init()
-    engine.setProperty('rate', 110)
+    engine.setProperty('rate', 140)
     engine.setProperty('voice', 'english+f1') # linux only m9
     engine.say(text)
     engine.runAndWait()
