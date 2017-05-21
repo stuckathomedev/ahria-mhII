100c2ec5e34989b26489e8ef5f07b8e96009826e
Kevin Liu <kevin@potatofrom.space>
2017-05-21 02:40:54 -0400

    Add sucky google assistant code

--- configure.py
+++ configure.py
@@ -1,8 +1,15 @@
 import base64
+import fcntl
+from subprocess import Popen, PIPE
 import json
 import webbrowser
 
 import tweepy
+import os
+
+
+
+
 
 
 TWEEPY_CONSUMER_KEY = "RksIJD0x7kb2toj5vJMOmA8IO"
@@ -37,8 +44,43 @@ def authorize_twitter():
     }
 
 
+def authorize_google_assistant():
+    def set_fd_nonblocking(fd):
+        """
+        Set the file description of the given file descriptor to non-blocking.
+        """
+        flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        flags = flags | os.O_NONBLOCK
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+
+    p = Popen("google-oauthlib-tool --client-secrets "
+              "./cogs/hotword/client_secret_940842120674-3vjlihqggtr5hed4open7aabbakn0msc.apps.googleusercontent.com.json "
+              "--scope https://www.googleapis.com/auth/assistant-sdk-prototype "
+              "--save --headless", stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=1)
+    set_fd_nonblocking(p.stdout)
+    set_fd_nonblocking(p.stderr)
+
+    p.stdin.write("1\n")
+    while True:
+        try:
+            out1 = p.stdout.read()
+        except IOError:
+            continue
+        else:
+            break
+    out1 = p.stdout.read()
+    p.stdin.write("5\n")
+    while True:
+        try:
+            out2 = p.stdout.read()
+        except IOError:
+            continue
+        else:
+            break
+
 def main():
     twitter_keys = authorize_twitter()
+    authorize_google_assistant()
     with open('data.json', 'w') as file:
         file.write(json.dumps({'twitter': twitter_keys}))
         print("Saved initial data to data.json.")
