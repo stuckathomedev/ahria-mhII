340b1824f904a8073941766cb3d6f3898902db1a
RektInPieces <kunalsharda@live.com>
2017-05-21 02:57:58 -0400

    Weather :D

--- cogs/weather/updated_weather.py
+++ cogs/weather/updated_weather.py
@@ -33,13 +33,19 @@ def send_text(phone_number,place):
     observation = owm.weather_at_place(place)
 
     temp = observation.get_weather().get_temperature('fahrenheit')
+
+
     account_sid = "AC984fee9fe6cc06c84923b4466a0c99a6"
     auth_token = "f107dd35e35b59857bbe03917ee1f83e"
     client = Client(account_sid, auth_token)
 
     message = client.api.account.messages.create(to="+1" + phone_number,
                                                  from_="+19788493104 ",
-                                                 body=temp)
+                                                 )
+
+get_weather("Boston, MA")
+
+
 
 
 
