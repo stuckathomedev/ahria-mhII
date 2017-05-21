2d4afb10662bb238e6aa942bff5667cb06436222
Kevin Liu <kevin@potatofrom.space>
2017-05-21 03:09:38 -0400

    Update hotword.py

--- cogs/hotword/hotword.py
+++ cogs/hotword/hotword.py
@@ -1,14 +1,29 @@
 import snowboydecoder
+import speech_recognition as sr
 
-detector = snowboydecoder.HotwordDetector()
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Please wait. Calibrating microphone...")
+    # listen for 5 seconds and create the ambient noise energy level
+    r.adjust_for_ambient_noise(source, duration=5)
 
-assistant = None
+    print("Done.")
 
-def callback():
-    detector.terminate()
-    assistant.start_conversation()
+    detector = snowboydecoder.HotwordDetector()
+
+    assistant = None
+
+    def callback():
+        detector.terminate()
+        r.listen(source)
+        try:
+            print(r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+        detector.start(detected_callback=callback, sleep_time=0.03)
+
     detector.start(detected_callback=callback, sleep_time=0.03)
-
-detector.start(detected_callback=callback, sleep_time=0.03)
-# blocks?
-detector.terminate()
+    # blocks?
+    detector.terminate()
