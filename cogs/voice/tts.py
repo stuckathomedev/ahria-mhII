85ea5a865216438b52e2c539d6e7e031cc5dfe9d
kiwkii <aneeshedara@gmail.com>
2017-05-21 08:56:53 -0400

    aAdded features to main bot file

--- cogs/voice/tts.py
+++ cogs/voice/tts.py
@@ -1,47 +1,5 @@
-import os
-
-import pygame
-from gtts import gTTS
-import os
-from pygame import mixer
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-
-def tts_set(message: str, language : str):
-    tts = gTTS(text=message, lang=language)
-    return tts
-
-
-def tts_save(tts, file_name: str):
-    tts.save(dir_path + "/" + file_name + ".mp3")
-
-
-def tts_error():
-    error_tts = tts_set("Sorry, an error has occured! Please try again!", 'en')
-    tts_save(error_tts, 'error')
-
-    mixer.init()
-    mixer.music.load(os.path.join(dir_path, "error.mp3"))
-    mixer.music.play()
-
-
-def tts_play(file_name_pre):
-    while True:
-        try:
-            file_name = file_name + ".mp3"
-            mixer.init()
-            mixer.music.load(file_name)
-            mixer.music.play()
-        except pygame.error:
-            tts_error()
-            continue
-        break
-
-
-def tts_auto_play(message : str, language : str, file_name : str):
-    tts_output = tts_set(message, str)
-    tts_save(tts_output, file_name)
-    tts_play(file_name)
-
+import pyttsx
+engine = pyttsx.init()
+engine.say('Sally sells seashells by the seashore.')
+engine.say('The quick brown fox jumped over the lazy dog.')
+engine.runAndWait()
