183f9525dec727c89a7694e9a5aa6759d5777596
Kevin Liu <kevin@potatofrom.space>
2017-05-21 10:07:50 -0400

    Female voice?

--- cogs/voice/tts.py
+++ cogs/voice/tts.py
@@ -3,5 +3,7 @@ import pyttsx
 
 def speak(text):
     engine = pyttsx.init()
+    engine.setProperty('rate', 110)
+    engine.setProperty('voice', 'english+f1') # linux only m9
     engine.say(text)
     engine.runAndWait()
