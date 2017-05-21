6532fc70bb4b84c7796ed97a398c619b22df8cd7
RektInPieces <kunalsharda@live.com>
2017-05-21 08:48:55 -0400

    Use different speech recognition provider

--- cogs/hotword/hotword.py
+++ cogs/hotword/hotword.py
@@ -4,8 +4,6 @@ import speech_recognition as sr
 
 def listen_forever(text_callback):
     r = sr.Recognizer()
-    print("Done.")
-
     detector = snowboydecoder.HotwordDetector(
         "resources/Ahria.pmdl", sensitivity=0.75)
 
@@ -14,9 +12,9 @@ def listen_forever(text_callback):
         detector.terminate()
         print("Heard ahria")
         with sr.Microphone() as source:
-            audio = r.listen(source)
+            audio = r.listen(source, timeout=5.0)
             try:
-                text_callback(r.recognize_sphinx(audio))
+                text_callback(r.recognize_google(audio))
             except sr.UnknownValueError:
                 print("Sphinx could not understand audio")
             except sr.RequestError as e:
